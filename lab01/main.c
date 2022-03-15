489980643a7d3844e78383f7eac61e471c1f20f5
maati01 <mateuszpow7@gmail.com>
2022-03-15 15:36:46 -0400

    new start

--- lab01/main.c
+++ lab01/main.c
@@ -1,6 +1,6 @@
 #include "roman.c"
 
 int main(int argc, char** argv){
-    count_lines_and_chars(1, argv[2],"tmp.txt");
+    count_lines_and_chars(argc, argv);
     return 0;
 }
--- lab01/roman.c
+++ lab01/roman.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 char** blocks;
-char char_;
-int number_of_lines = 0;
-int number_of_chars = 0;
-int number_of_words = 0;
-int results[3];
+char* temporary_file_name = "tmp.txt";
+
+FILE* file;
 
 void create_table(int size_array){
     blocks = calloc(size_array,sizeof(char*));
@@ -18,44 +17,15 @@ void remove_block(int index){
     free(blocks[index]);
 }
 
-void count_lines_and_chars(int size, char* files, char* temporary_file_name){
-    FILE* file;
+void count_lines_and_chars(int size, char** files){
+    for(int i = 2; i < size; i++){
+        file = fopen(files[i], "r");
+        char command[256];
 
-    for(int i = 0; i < size; i++){
-        file = fopen(files, "r");
-        char_ = getc(file);
-
-        while (char_ != EOF){
-            if (char_ == '\n'){
-                number_of_lines += 1;
-            }
-            if (isspace(char_)){
-                number_of_words += 1;
-            }
-            else{
-                number_of_chars += 1;
-            }
-
-            char_ = getc(file);
-        }
+        snprintf(command, sizeof(command),"wc %s > %s", files[i], temporary_file_name);
+        system(command);
 
         fclose(file);
-    }
-
-
-    results[0] = number_of_lines;
-    results[1] = number_of_chars;
-    results[2] = number_of_words;
-
-
-    FILE* temporary_file = fopen(temporary_file_name,"w");
-
-    printf("%s",files);
-    printf("\n");
-    printf("%s",temporary_file_name);
-    fprintf(temporary_file,"%d %d %d\n",results[0],results[1],results[2]);
-    fprintf(temporary_file,"%d %d %d",results[0],results[1],results[2]);
-
-    fclose(temporary_file);
-    
+        
+    }    
 }
