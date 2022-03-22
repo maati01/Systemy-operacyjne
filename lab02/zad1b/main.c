ddfab5178644a85a05f19191d1b29dc022e6d5d7
maati01 <mateuszpow7@gmail.com>
2022-03-22 12:17:57 -0400

    finished task 1b

--- lab02/zad1b/main.c
+++ lab02/zad1b/main.c
@@ -4,10 +4,12 @@
 #include <string.h>
 #include <sys/times.h>
 #include <unistd.h>
+#include <fcntl.h> 
 
-FILE* file;
-FILE* result;
-char curr_char[1];
+
+int file;
+int result;
+char curr_char;
 int exists_char_in_line = 0;
 int line_length = 0;
 int const char_size = sizeof(char);
@@ -18,8 +20,8 @@ clock_t end_time;
 
 struct tms start_tms;
 struct tms end_tms;
-char stats_file[] = "pomiar_zad_1a.txt";
-char version[] = "library version\n";
+char stats_file[] = "pomiar_zad_1b.txt";
+char version[] = "system version\n";
 
 
 void print_and_save_stats(double real_time, double user_time, double system_time){
@@ -27,9 +29,9 @@ void print_and_save_stats(double real_time, double user_time, double system_time
     char* user_time_output = calloc(21, sizeof(char));
     char* sys_time_output = calloc(20, sizeof(char));
 
-    snprintf(real_time_output, 21,"Real time: %f\n", real_time);
-    snprintf(user_time_output, 21,"User time: %f\n", user_time);
-    snprintf(sys_time_output, 20,"Sys time: %f\n", system_time);
+    snprintf(real_time_output, 21, "Real time: %f\n", real_time);
+    snprintf(user_time_output, 21, "User time: %f\n", user_time);
+    snprintf(sys_time_output, 20, "Sys time: %f\n", system_time);
 
     FILE *output = fopen(stats_file, "a");
     fputs(version, output);
@@ -65,18 +67,25 @@ void end_timer(){
     print_and_save_stats(real_time, user_time, system_time);
 }
 
-void back_pointer_and_save(FILE* input, FILE* output, int length){
+void back_pointer_and_save(int input, int output, int length){
     line = calloc(length, char_size);
-    fseek(input, -length, 1);
-    fread(line, char_size, length, input);
-    fwrite(line, char_size, length, output);
+    lseek(input, -length, SEEK_CUR);
+    read(input, line, length);
+    write(output, line, length);
     free(line);
 }
 
 int input_verification(int argc, char** argv){
-    file = fopen(argv[1], "r");
-    result = fopen(argv[2], "r+");
-    if(file == NULL || result == NULL || argc != 3){
+    char* input_path = calloc(strlen(argv[1]) + 3, sizeof(char));
+    char* output_path = calloc(strlen(argv[2]) + 3, sizeof(char));
+    snprintf(input_path, strlen(argv[1]) + 3,"./%s", argv[1]);
+    snprintf(output_path, strlen(argv[2]) + 3,"./%s", argv[2]);
+
+    file = open(input_path, O_RDONLY);
+    result = open(output_path, O_WRONLY);
+    free(input_path);
+    free(output_path);
+    if(file == -1 || result == -1 || argc != 3){
         char file1[256];
         char file2[256];
         printf("Wrong input!\nEnter the name of the file: \n");
@@ -88,13 +97,14 @@ int input_verification(int argc, char** argv){
         printf("The files are correct!\n");
         return 1;
     }
+
 }
 
 int main(int argc, char** argv){
     while(input_verification(argc,argv) == 0);
     start_timer();
-    while(fread(curr_char, char_size, 1, file) != 0){
-        if(curr_char[0] == '\n' && exists_char_in_line){
+    while(read(file, &curr_char, char_size) == 1){
+        if(curr_char == '\n' && exists_char_in_line){
             back_pointer_and_save(file, result, line_length+1);
             line_length = 0;
             exists_char_in_line = 0;
@@ -103,13 +113,14 @@ int main(int argc, char** argv){
 
         line_length++;
 
-        if(isspace(curr_char[0]) == 0){
+        if(isspace(curr_char) == 0){
             exists_char_in_line = 1;
         }
-        if(curr_char[0] == '\n'){
+        if(curr_char == '\n'){
             line_length = 0;
         }
         
+
     }
     
     if(exists_char_in_line){
@@ -118,7 +129,7 @@ int main(int argc, char** argv){
 
     printf("Lines copied successfully!\n");
     end_timer();
-    fclose(file);
-    fclose(result);
+    close(file);
+    close(result);
     return 0;
 }
