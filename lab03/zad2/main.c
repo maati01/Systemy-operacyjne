5c3c6ac8a9adabf0d41130132df527bdedd112be
maati01 <mateuszpow7@gmail.com>
2022-03-31 15:09:11 -0400

    timer

--- lab03/zad2/main.c
+++ lab03/zad2/main.c
@@ -3,7 +3,71 @@
 #include <math.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <sys/times.h>
 
+
+clock_t start_time;
+clock_t end_time;
+
+struct tms start_tms;
+struct tms end_tms;
+
+char result_file[] = "results2.txt";
+
+double calculate_time_in_second(clock_t start, clock_t end){
+    return (double)(end - start)/sysconf(_SC_CLK_TCK);
+}
+
+void print_and_save_stats(double diff, double user_time, double system_time, int n, double width, double result){
+    char* real_time_output = calloc(21,sizeof(char));
+    char* user_time_output = calloc(21,sizeof(char));
+    char* sys_time_output = calloc(20,sizeof(char));
+    char* input = calloc(100,sizeof(char));
+    char* result_string = calloc(25,sizeof(char));
+
+    snprintf(real_time_output, 21,"Real time: %f\n", diff);
+    snprintf(user_time_output, 21,"User time: %f\n", user_time);
+    snprintf(sys_time_output, 20,"Sys time: %f\n", system_time);
+    snprintf(input, 100,"Processes: %d   Width: %f\n", n, width);
+    snprintf(result_string, 25,"Result: %f\n", result);
+
+    FILE *output = fopen(result_file,"a");
+    fputs(input,output);
+	fputs(real_time_output,output);
+	fputs(user_time_output,output);
+	fputs(sys_time_output,output);
+    fputs(result_string,output);
+    fputs("\n", output);
+	fclose(output);
+
+    printf("%s", input);
+    printf("%s", real_time_output);
+    printf("%s", user_time_output);
+    printf("%s", sys_time_output);
+    printf("%s",result_string);
+    printf("\n");
+
+    free(real_time_output);
+    free(user_time_output);
+    free(sys_time_output);
+    free(input);
+    free(result_string);
+}
+
+void start_timer(){
+    start_time = times(&start_tms);
+}
+
+void end_timer(int n, double width, double result){
+    end_time = times(&end_tms);
+    double real_time =  calculate_time_in_second(start_time, end_time);
+    double user_time = calculate_time_in_second(start_tms.tms_cutime, end_tms.tms_cutime);
+    double system_time = calculate_time_in_second(start_tms.tms_cstime, end_tms.tms_cstime);
+
+    print_and_save_stats(real_time, user_time, system_time, n, width, result);
+
+}
 
 double func(double x){
     return ((double)4)/((double)(x*x + 1));
@@ -17,6 +81,8 @@ int main(int argc, char** argv){
     double width = strtod(argv[1],&end);
     int n = atoi(argv[2]);
     double area = (double)1/n;
+
+    start_timer();
 
     for(int i = 0; i < n; i++) {
         pid = fork();
@@ -35,9 +101,8 @@ int main(int argc, char** argv){
                 temp_result += width*func(j*width);
             }
 
-            FILE *output = fopen(result_file,"a");
+            FILE *output = fopen(result_file,"w+");
             fprintf(output, "%f", temp_result);
-            printf("Number of process: %d\n", i + 1);
             free(result_file);
             exit(0); 
         } else  {
@@ -56,6 +121,7 @@ int main(int argc, char** argv){
         free(result_file);
         free(value);
     }
-    printf("%lf\n", result);
+
+    end_timer(n, width, result);
     return 0;
 }
