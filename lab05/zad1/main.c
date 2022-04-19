4473c574f649e75e33743b07e7932e371d894c09
maati01 <mateuszpow7@gmail.com>
2022-04-19 19:41:40 -0400

    update

--- lab05/zad1/main.c
+++ lab05/zad1/main.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
 
 FILE* input;
 int lines = 0;
@@ -54,19 +57,54 @@ void read_commands(char** commands, int size){
 }
 
 void run_program(char** components, char** commands, int size){
+    int pipes[256][2];
+
+    for (int i = 0; i < 256; ++i) {
+        if (pipe(pipes[i]) < 0) {
+            exit(EXIT_FAILURE);
+        }
+    }
+
     for(int i = 0; i < size; i++){
-        int val;
-        // char* current_line = strtok(commands[i], " | ");
+        int commands_counter[256];
+        int size = 0;
 
         while (*commands[i]) { 
             if ( isdigit(*commands[i]) || ( (*commands[i]=='-'||*commands[i]=='+') && isdigit(*(commands[i]+1)) )) {
                 long val = strtol(commands[i], &commands[i], 10);
-                char* current_command = strtok(commands[val-1], " | ");
-                printf("%s\n", current_command); 
+                commands_counter[size] = val;
+                size++;
             } else {
                 commands[i]++;
             }
         }
+        for(int j = 0; j < size; j++){
+            pid_t pid = fork();
+            if(pid < 0){
+                exit(EXIT_FAILURE);
+            }else if(pid == 0){
+                if(j > 0){
+                    dup2(pipes[j - 1][0], STDIN_FILENO);
+                }
+                dup2(pipes[j][1], STDOUT_FILENO);
+            }
+            
+            for (int k = 0; k < size; k++){
+                close(pipes[k][0]);
+                close(pipes[k][1]);
+            }
+            // tutaj trzeba przygotowac argumenty 
+            execvp(components[commands_counter[j]][0],components[commands_counter[j]][0]);
+            exit(0);
+        }
+        for (int j = 0; j < size - 1; ++j){
+            close(pipes[j][0]);
+            close(pipes[j][1]);
+        }
+        for (int j = 0; j < size; ++j){
+            wait(0);
+        }
+
     }
 }
 
@@ -80,6 +118,10 @@ int main(int argc, char** argv){
     char** commands = calloc(number-1, sizeof(char*));
     read_commands(commands,number);
 
+    // printf("%s", commands[0]); 
+    // printf("%s", commands[1]); 
+    // printf("%s", commands[2]); 
+    // printf("%s", commands[3]); 
     run_program(components, commands, number);
     
     free(commands);
